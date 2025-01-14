25cff9fe17b004b2dbe49f8ef35dd3b3fb3ef7b2
childe <rmself@qq.com>
2025-01-14 11:20:57 +0800

    Add drawDice

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="gamebox" style="height: 600px; width: 600; border: solid black 1px"></div>
+  <div id="gamebox" style="height: 800px; width: 800px; border: solid black 1px"></div>
 </template>
 
 <script>
@@ -7,14 +7,47 @@ import { SVG } from '@svgdotjs/svg.js'
 import '@svgdotjs/svg.draggable.js'
 
 export default {
-  methods: {},
+  methods: {
+    drawDice(draw, x, y, number) {
+      const diceSize = 100 * 0.8
+      const diceX = 100 * 0.1 + x * 100
+      const diceY = 100 * 0.1 + y * 100
+
+      // 画色子的背景
+      draw.rect(diceSize, diceSize).move(diceX, diceY).fill('#f80')
+
+      // 画色子上的数字
+      const fontSize = diceSize * 0.5
+      draw
+        .text(number)
+        .font({
+          size: fontSize,
+          family: 'Arial',
+          fill: '#ffffff',
+        })
+        .move(diceX + diceSize / 2 - fontSize / 2, diceY + diceSize / 2 - fontSize / 2)
+    },
+  },
   mounted() {
     const constraints = {
-      x: { min: 0, max: 600 },
-      y: { min: 0, max: 600 },
+      x: { min: 0, max: 800 },
+      y: { min: 0, max: 800 },
     }
     var draw = SVG().addTo('#gamebox').size(constraints.x.max, constraints.y.max)
-    var rect = draw.rect(100, 100).move(100, 100).attr({ fill: '#eee' })
+    // 画棋盘
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 6; j++) {
+        const color = (i + j) % 2 === 0 ? '#ffffff' : '#cccccc'
+        draw
+          .rect(100, 100)
+          .move(j * 100, i * 100)
+          .fill(color)
+      }
+    }
+    // 画红色色子
+    this.drawDice(draw, 2, 3, 'c4')
+
+    var rect = draw.rect(100, 100).move(700, 0).attr({ fill: '#bbb' })
     rect.draggable()
 
     rect.on('dragmove.namespace', (e) => {
